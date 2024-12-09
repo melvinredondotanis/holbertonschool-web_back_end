5c81f37208c77fa19626938736f426912cba833d
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-12-09 21:52:46 +0100

    refactor: remove unnecessary newline in element_length function

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -8,4 +8,3 @@ from typing import Sequence, List, Tuple, Iterable
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """return a sequence and int inside a tuple that inside a list"""
     return [(i, len(i)) for i in lst]
-
